26caa603825d3aa1b3c0344abacf0a7129825209
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-06-29 21:57:03 -0300

    Led Matrix Page

--- ardufield/src/pages/ledMatrix/index.js
+++ ardufield/src/pages/ledMatrix/index.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { View, Text } from 'react-native';
+import { Switch, StyleSheet, Text } from 'react-native';
 
-// import styles from './styles';
+import { 
+  Container,
+  Title,
+} from './styles';
 
-const LedMatrix = () => (
-  
-    <Text>Tela da Matriz de LED</Text>
+export default class LedMatrix extends Component {
 
-);
+  state  = { switchValue: false };
 
-export default LedMatrix;
+
+  toggleSwitch = (value) => {
+    this.setState({ switchValue: value });
+  }
+
+
+  render() {
+    return (
+      <Container>
+        <Title>Controle a Matriz:</Title>
+        
+        <Text style={[styles.subtitle, this.state.switchValue ? styles.textON : styles.textOFF ]}>
+            {this.state.switchValue ? 'Agora, ela está: LIGADA' : 'Agora, ela está: DESLIGADA'}
+        </Text>
+
+        <Switch style={styles.switch}
+            onValueChange={this.toggleSwitch}
+            value={this.state.switchValue}
+        />
+      </Container>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  switch: {
+      transform: [{ scaleX: 2.0 }, { scaleY: 2.0 }]
+  }, 
+  subtitle: {
+    fontSize: 24,
+    marginBottom: 20
+  },
+  textON: {
+    color: '#a1b67d'
+  },
+  textOFF: {
+    color: '#c3c0c5'
+  }
+})
---
+++ ardufield/src/pages/ledMatrix/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Container = styled.View`
+  flex: 1;
+  alignItems: center;
+  justifyContent: center;
+  backgroundColor: #F5F5F5;
+`;
+
+const Title = styled.Text`
+    fontSize: 44;
+    marginBottom: 80px;
+    color: #d13e3b;
+`;
+
+export { Container, Title };
